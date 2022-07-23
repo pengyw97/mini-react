a65f5bd147e2a15a7306d03c9def6fa25223d5c3
pengyw97 <877285030@qq.com>
2022-07-23 10:56:18 +0800

    feat: first commit

---
+++ demo/which-react.js
@@ -0,0 +1,7 @@
+import React, { useReducer, Component } from "react";
+import ReactDOM from "react-dom";
+
+// import { useReducer } from "../src/react";
+// import ReactDOM from "../src/react-dom";
+
+export { ReactDOM, useReducer, Component };
